d5f040ef3087dcc82a7c17be564e6b957387151c
Delaney Smith <63112746+delaneysmith22@users.noreply.github.com>
2023-02-02 14:41:51 -0500

    Update run_model.m to allow for Losartan simulations

--- run_model.m
+++ run_model.m
@@ -5,16 +5,16 @@
     % units: units of dose (ng/kg/min, ng/min)
     % inf_type: type of infusion (none, SC, IV)
     % separate: whether to separate endogenous and exogenous Ang II (true, false)
-function [S,t,YP] = run_model(days,x0,D,units,inf_type,separate,varargin)
+function [S,t,YP] = run_model(days,x0,x_p0, D,units,inf_type,separate,varargin)
     
     % Time resolution (mins/day)
     t_res = 1440;
 
     % Number of points for plotting resolution
-    N = ((days)*t_res)*10;
+    N = ((days)*t_res)*2;
     
     % Initial time (min); Final time (min); Time vector
-    t0 = 0*t_res; tend = days*t_res; tspan = linspace(t0,tend,N);%%[t0 tend];%
+    t0 = 0*t_res; tend = days*t_res; tspan = linspace(t0,tend,N);
     
     % Load parameters
     pars = get_pars;
@@ -33,23 +33,29 @@ function [S,t,YP] = run_model(days,x0,D,units,inf_type,separate,varargin)
     if ~isempty(varargin)
     varargin_input = varargin{:};
     end
+
+    num_vars = 0;
+    for i = 1:length(varargin_input)
+        if strcmp(varargin_input{i},'losartan')
+            num_vars = 26;
+            break;
+        end
+    end
     
     if separate
-        num_vars = 74;
+        num_vars = num_vars + 75;
     else
-        num_vars = 40;
+        num_vars = num_vars + 41;
     end
-       
-    x_p0 = zeros(num_vars,1);
     
-    options = odeset('RelTol',1e-6,'AbsTol',1e-3);
+    options = odeset('RelTol',1e-6,'AbsTol',1e-4);
     
     sol = ode15i(@(t,x,x_p) ...
                 model(t,x,x_p,pars,infusion,varargin_input{:}), ...
                             tspan, x0, x_p0,options);  
-    S = sol.y;
-    t_ = sol.x;
+
     [Y,YP] = deval(sol,tspan);
-    t = t_'/t_res;
+    S = Y;
+    t = tspan/t_res;
 
 end
